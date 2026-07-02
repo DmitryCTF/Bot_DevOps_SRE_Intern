7e004310e02606197be78f2be09afef707d093c6
DmitryCTF <minidima23@gmail.com>
2026-07-02 18:54:32 +0500

    Добавил загрузку Telegram настроек из env

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import keyword
-
+import os
+from dotenv import load_dotenv
 from telethon import TelegramClient, events
 
+load_dotenv()
+api_id_raw = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+
+if api_id_raw is None:
+    raise RuntimeError("Не найден API_ID. Проверь файл .env")
+
+if api_hash is None:
+    raise RuntimeError("Не найден API_HASH. Проверь файл .env")
+
+api_id = int(api_id_raw)
+print("Telegram-настройки успешно загружены")
+print(f"API_ID: {api_id}")
+print(f"API_HASH длина: {len(api_hash)} символов")
 
 def find_keywords(post_text, keywords):
     post_text_lower = post_text.lower()
